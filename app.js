1e952c56464a2edf6dedaa06eef1217848910836
morgween <105385179+morgween@users.noreply.github.com>
2022-10-22 14:52:56 +0300

    Create project and setup app.js file

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app=express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/",(req,res)=>{
+    res.send("Hello")
+})
+
+
+app.listen(3000,()=>{
+    console.log("Server started on port 3000")
+})
