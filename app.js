07d50bea3b67561e9b11b31c2932822e89fecd3d
morgween <105385179+morgween@users.noreply.github.com>
2022-10-22 15:32:31 +0300

    import ejs

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
 const app=express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.set(ejs);
 
 
 app.get("/",(req,res)=>{
