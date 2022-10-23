2b6d71a4ecaa43df8f6c2bb7bb8f2011ef9c4ebc
morgween <105385179+morgween@users.noreply.github.com>
2022-10-23 11:47:32 +0300

    give starting functionallity

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const ejs = require("ejs");
 
 const app=express();
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-app.set(ejs);
+
+var items = [];
+
+
+app.set("view engine","ejs");
+
+
+app.post("/",(req,res)=>{
+    items.push(req.body.newItem);
+    res.redirect("/");
+});
 
 
 app.get("/",(req,res)=>{
-    res.send("Hello")
+    let today = new Date();
+
+    let options={
+        weekday:'long',
+        day:'numeric',
+        month:'long'
+    };
+
+    res.render("list", {kindOfDay : today.toLocaleDateString("en-US", options), newListItems : items});
 })
 
 
