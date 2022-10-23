f8eb8cd5616d2059331faa3c79961855567602cd
morgween <105385179+morgween@users.noreply.github.com>
2022-10-23 12:14:37 +0300

    working on project

--- app.js
+++ app.js
@@ -7,14 +7,21 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
 var items = [];
+var workItems = [];
 
 
 app.set("view engine","ejs");
 
 
 app.post("/",(req,res)=>{
-    items.push(req.body.newItem);
-    res.redirect("/");
+    if (req.body.list ==="Work"){
+        workItems.push(req.body.newItem);
+        res.redirect("/work");
+    }
+    else {
+        items.push(req.body.newItem);
+        res.redirect("/");
+    }
 });
 
 
@@ -27,10 +34,15 @@ app.get("/",(req,res)=>{
         month:'long'
     };
 
-    res.render("list", {kindOfDay : today.toLocaleDateString("en-US", options), newListItems : items});
-})
+    res.render("list", {listTitle : today.toLocaleDateString("en-US", options), newListItems : items});
+});
+
+app.get("/work", (req,res)=>{
+    res.render("list", {listTitle : "Work List", newListItems:workItems});
+});
+
 
 
 app.listen(3000,()=>{
-    console.log("Server started on port 3000")
-})
+    console.log("Server started on port 3000");
+});
